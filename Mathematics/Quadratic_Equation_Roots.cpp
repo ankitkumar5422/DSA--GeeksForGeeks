53a7e6e19e93ef5670416747a6f856ccd2aa869a
Ankit Kumar <61801306+ankitkumar5422@users.noreply.github.com>
2021-06-13 14:13:25 +0530

    Update Quadratic_Equation_Roots.cpp

--- Mathematics/Quadratic_Equation_Roots.cpp
+++ Mathematics/Quadratic_Equation_Roots.cpp
@@ -15,5 +15,6 @@ void quadraticRoots(int a,int b, int c)
          float r1 = floor((-b+sqrt(D))/(2.0*a));
          float r2 = floor((-b-sqrt(D))/(2.0*a));
          cout<<r1<<" "<<r2;
+       
      }
 }
